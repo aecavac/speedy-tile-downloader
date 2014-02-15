37e6fcf99dc924ef04f927125fbe34373ed3b28b
Ali Emre CAVAC <aecavac@gmail.com>
2014-02-16 00:20:48 +0200

    Init
    Initial commit.

---
+++ src/TileDownloader.java
@@ -0,0 +1,303 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * TMS tile downloader thread class. 
+ * 
+ * @author ali.cavac
+ *
+ */
+public class TileDownloader implements Runnable {
+
+	public TileDownloader() {
+	}
+
+	// Variables
+	private String tileService;
+	private String destPath;
+
+	private int zoomLevelStartIndex = 0;
+	private int zoomLevelEndIndex = 0;
+
+	private int xStartIndex = 0;
+	private int yStartIndex = 0;
+
+	private int xEndIndex = 0;
+	private int yEndIndex = 0;
+
+	// Global Statistic Parameters
+	public static int downImageCount = 0;
+
+	/**
+	 * Download tiles.
+	 */
+	private void downloadTiles() {
+
+		String destImgPath = "";
+		String destImgName = "";
+
+		String tileServiceImg = "";
+
+		for (int z = zoomLevelStartIndex; z <= zoomLevelEndIndex; z++) {
+
+			int maxXOfCurrentZoomLevel;
+			int maxYOfCurrentZoomLevel;
+			int xStart;
+			int yStart;
+			int xEnd;
+			int yEnd;
+
+			maxXOfCurrentZoomLevel = (int) (Math.pow(2, z));
+			maxYOfCurrentZoomLevel = (int) (Math.pow(2, z));
+
+			// X start
+			if ((z == zoomLevelStartIndex) && (xStartIndex != 0)) {
+				xStart = xStartIndex;
+			} else {
+				xStart = 0;
+			}
+
+			// X end
+			if ((z == zoomLevelEndIndex) && (xEndIndex != 0)) {
+				xEnd = xEndIndex;
+			} else {
+				xEnd = maxXOfCurrentZoomLevel - 1;
+			}
+
+			for (int x = xStart; x <= xEnd; x++) {
+
+				// Y Start
+				if ((z == zoomLevelStartIndex) && (x == xStart)
+						&& (yStartIndex != 0)) {
+					yStart = yStartIndex;
+				} else {
+					yStart = 0;
+				}
+
+				// Y end
+				if ((z == zoomLevelEndIndex) && (x == xEnd) && (yEndIndex != 0)) {
+					yEnd = yEndIndex;
+				} else {
+					yEnd = maxYOfCurrentZoomLevel - 1;
+				}
+
+				for (int y = yStart; y <= yEnd; y++) {
+
+					destImgPath = getDestPath() + "/" + z + "/" + x;
+					destImgName = y + ".png";
+					tileServiceImg = getTileService() + "/" + z + "/" + x + "/" + y
+							+ ".png";
+					
+					//Check if image exist.
+					File checkFile = new File(destImgPath + "/" + destImgName);
+					if(checkFile.exists()){
+//						System.out.println("EXIST : " + destImgPath + "/" + destImgName);
+						continue;
+					}
+//					System.out.println("DOWNLOAD : " + destImgPath + "/" + destImgName);
+
+					final byte[] imgBytes;
+					
+					if(this.getTileService().toUpperCase().startsWith("HTTPS")){
+						imgBytes = this.getImageByteArrayHTTPS(tileServiceImg);
+					}else{
+						imgBytes = this.getImageByteArrayHTTP(tileServiceImg);
+					}
+					
+					if (imgBytes == null) {
+						// TODO : SAVE & TERMINATE || WAIT TO GET CONN
+						return;
+					}
+
+					final boolean writeResult = this.writeImageToDestination(
+							destImgPath, destImgName, imgBytes);
+
+					if (!writeResult) {
+						// TODO : SAVE & TERMINATE || WAIT TO GET CONN
+						return;
+					}
+
+					downImageCount++;
+
+					// Clean
+					destImgPath = "";
+					destImgName = "";
+				}
+
+				// Clean
+				yStartIndex = 0;
+			}
+
+			// Clean
+			xStartIndex = 0;
+		}
+		System.out.println("COMPLATED!");
+	}
+
+	/**
+	 * Download image from http service.
+	 * @param urlStr
+	 * @return
+	 */
+	private byte[] getImageByteArrayHTTP(final String urlStr) {
+		try {
+			final URL url = new URL(urlStr);
+			final InputStream in = new BufferedInputStream(url.openStream());
+			final ByteArrayOutputStream out = new ByteArrayOutputStream();
+			final byte[] buf = new byte[1024];
+			int n = 0;
+			while (-1 != (n = in.read(buf))) {
+				out.write(buf, 0, n);
+			}
+			out.close();
+			in.close();
+			final byte[] response = out.toByteArray();
+			return response;
+		} catch (final Exception e) {
+			System.out.println("getImageByteArrayHTTP:");
+			System.out.println(urlStr);
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Download image from https service.
+	 * @param urlStr
+	 * @return
+	 */
+	private byte[] getImageByteArrayHTTPS(final String urlStr) {
+		try {
+			URL url = new URL(urlStr);
+			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+			if (con == null){
+				return null;
+			}
+			
+			final InputStream in = con.getInputStream();
+			final ByteArrayOutputStream out = new ByteArrayOutputStream();
+			final byte[] buf = new byte[1024];
+			int n = 0;
+			while (-1 != (n = in.read(buf))) {
+				out.write(buf, 0, n);
+			}
+			
+			out.close();
+			in.close();
+			final byte[] response = out.toByteArray();
+			return response;						
+		} catch (final Exception e) {
+			System.out.println("getImageByteArrayHTTPS:");
+			System.out.println(urlStr);
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Write image to destination.
+	 * 
+	 * @param destinationPath
+	 * @param imageName
+	 * @param image
+	 * @return
+	 */
+	private boolean writeImageToDestination(final String destinationPath,
+			final String imageName, final byte[] image) {
+		try {
+			final File file = new File(destinationPath);
+			file.mkdirs();
+
+			final FileOutputStream fos = new FileOutputStream(destinationPath
+					+ "/" + imageName);
+			fos.write(image);
+			fos.close();
+			return true;
+		} catch (final Exception e) {
+			System.out.println("writeImageToDestination:");
+			System.out.println(destinationPath);
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public String getTileService() {
+		return tileService;
+	}
+
+	public void setTileService(String tileService) {
+		this.tileService = tileService;
+	}
+
+	public String getDestPath() {
+		return destPath;
+	}
+
+	public void setDestPath(String destPath) {
+		this.destPath = destPath;
+	}		
+
+	public int getZoomLevelStartIndex() {
+		return zoomLevelStartIndex;
+	}
+
+	public void setZoomLevelStartIndex(int zoomLevelStartIndex) {
+		this.zoomLevelStartIndex = zoomLevelStartIndex;
+	}
+
+	public int getZoomLevelEndIndex() {
+		return zoomLevelEndIndex;
+	}
+
+	public void setZoomLevelEndIndex(int zoomLevelEndIndex) {
+		this.zoomLevelEndIndex = zoomLevelEndIndex;
+	}
+
+	public int getxStartIndex() {
+		return xStartIndex;
+	}
+
+	public void setxStartIndex(int xStartIndex) {
+		this.xStartIndex = xStartIndex;
+	}
+
+	public int getyStartIndex() {
+		return yStartIndex;
+	}
+
+	public void setyStartIndex(int yStartIndex) {
+		this.yStartIndex = yStartIndex;
+	}
+
+	public int getxEndIndex() {
+		return xEndIndex;
+	}
+
+	public void setxEndIndex(int xEndIndex) {
+		this.xEndIndex = xEndIndex;
+	}
+
+	public int getyEndIndex() {
+		return yEndIndex;
+	}
+
+	public void setyEndIndex(int yEndIndex) {
+		this.yEndIndex = yEndIndex;
+	}
+	
+	@Override
+	public void run() {
+		this.downloadTiles();
+	}
+}
---
+++ src/TileDownloaderTester.java
@@ -0,0 +1,166 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class TileDownloaderTester {
+	
+	/**
+	 * Configuration variables.
+	 */
+	private final static String tileService = "https://a.tiles.mapbox.com/v3/foursquare.meku766r";
+	private final static String destPath = "C:/osm_tiles";
+	private final static int zoomStart = 0;
+	private final static int zoomEnd = 2;
+	
+	public static void main(String[] args) {
+		TileDownloaderTester tileDownloaderTester = new TileDownloaderTester();
+		//tileDownloaderTester.testSingleThread(tileService, destPath, zoomStart, zoomEnd);
+		tileDownloaderTester.testMultiThread(tileService, destPath, zoomStart, zoomEnd);
+	}
+	
+	/**
+	 * Single Thread Test
+	 */	
+	private void testSingleThread(int zStart, int zEnd) {
+		
+		int zoomStart = zStart;
+		int zoomEnd = zEnd;
+		
+		Date startDate = new Date();
+		
+		TileDownloader tileDownloader = new TileDownloader();
+		
+		tileDownloader.setTileService(tileService);
+		tileDownloader.setDestPath(destPath);
+
+		tileDownloader.setZoomLevelStartIndex(zoomStart);
+		tileDownloader.setZoomLevelEndIndex(zoomEnd);
+
+		tileDownloader.setxStartIndex(0);
+		tileDownloader.setyStartIndex(0);
+
+		tileDownloader.setxEndIndex(0);
+		tileDownloader.setyEndIndex(0);
+
+		Thread tileDownloaderThread = new Thread(tileDownloader);
+		tileDownloaderThread.start();
+		
+		try {
+			tileDownloaderThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		Date endDate = new Date();
+
+		long diffMill = endDate.getTime() - startDate.getTime();
+		System.out.println("==============ZOOM " + zoomStart + "-" + zoomEnd + "==============");
+		System.out.println("Time : " + diffMill);
+		System.out.println("Img Count : " + TileDownloader.downImageCount);
+		System.out.println("Avg : " + diffMill / TileDownloader.downImageCount);
+		System.out.println("=====================================");
+		
+	}
+	
+	/**
+	 * Multi Thread Test
+	 * 
+	 * ZOOM : 0, 1, 2 	Thd : 1
+	 * ZOOM : 3 		Thd : 3 ( 2^3/2 -1 )
+	 * ZOOM : 4 		Thd : 7 ( 2^4/2 -1 )
+	 * ZOOM : 5 		Thd : 15( 2^5/2 -1 )
+	 * ZOOM : X >= 6	Thd : 32
+	 */
+	private void testMultiThread(final String tileService, final String destPath, int zStart, int zEnd) {
+		
+		int zoomStart = zStart;
+		int zoomEnd = zEnd;
+		
+		int numberOfThread = 0;
+		Date startDate = new Date();
+
+		// Divide works to threads
+		List<Thread> threads = new ArrayList<>();
+
+		for (int z = zoomStart; z <= zoomEnd; z++) {
+
+			System.out.println("Zoom : " + z);
+			
+			//Calculate number of thread
+			if(z < 3){
+				numberOfThread = 1;
+			}else if( z < 6 ){
+				numberOfThread = (int)Math.pow(2, z-1) - 1;
+			}else{
+				numberOfThread = 32;
+			}
+			
+			int totalXDirectoryNum = (int) (Math.pow(2, z));
+
+			for (int xStart = 0; xStart < totalXDirectoryNum; ) {
+				int xEnd;
+				
+				//If directory num enough for thread num.
+				if(totalXDirectoryNum / numberOfThread > 1){
+					xEnd = xStart + totalXDirectoryNum / numberOfThread - 1;
+				}else{
+					//If not then download all dirs.
+					xEnd = 0;	
+				}
+				if(xEnd >= totalXDirectoryNum){
+					xEnd = totalXDirectoryNum - 1;
+				}
+
+				TileDownloader tileDownloader = new TileDownloader();
+				
+				tileDownloader.setTileService(tileService);
+				tileDownloader.setDestPath(destPath);
+
+				tileDownloader.setZoomLevelStartIndex(z);
+				tileDownloader.setZoomLevelEndIndex(z);
+
+				tileDownloader.setxStartIndex(xStart);
+				tileDownloader.setyStartIndex(0);
+
+				tileDownloader.setxEndIndex(xEnd);
+				tileDownloader.setyEndIndex(0);
+
+				System.out.println("xStart:" + xStart + " xEnd:" + xEnd);
+				Thread tileDownloaderThread = new Thread(tileDownloader);
+				tileDownloaderThread.start();
+				threads.add(tileDownloaderThread);
+				
+				if(totalXDirectoryNum / numberOfThread > 1){
+					xStart += totalXDirectoryNum / numberOfThread;
+				}else{
+					//Aktif zoom da devam etmemesi için..
+					xStart = totalXDirectoryNum;	
+				}
+			}
+			
+			//Wait threads
+			System.out.println("List size : " + threads.size());
+			for (int i = 0; i < threads.size(); i++)
+		    {
+				try {
+					((Thread)threads.get(i)).join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		    }
+			
+			System.out.println("---Clear Thread List---");
+			threads.clear();
+		}
+
+		Date endDate = new Date();
+
+		long diffMill = endDate.getTime() - startDate.getTime();
+		System.out.println("==============ZOOM " + zoomStart + "-" + zoomEnd + "==============");
+		System.out.println("Time : " + diffMill);
+		System.out.println("Img Count : " + TileDownloader.downImageCount);
+		System.out.println("Avg : " + diffMill / TileDownloader.downImageCount);
+		System.out.println("====================================");
+	}
+}
